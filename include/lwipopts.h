334be823507b9e146265bc2c84b1e2c5c12b8c48
Hendrik <john.dalmas999@gmail.com>
2024-06-29 11:26:57 +0200

    clean again

--- include/lwipopts.h
+++ include/lwipopts.h
@@ -6,9 +6,4 @@
 //
 // This example uses a common include to avoid repetition
 #include "lwipopts_examples_common.h"
-<<<<<<< HEAD
-=======
-// #define WIFI_SSID "@WIFI_SSID@"
-// #define WIFI_PASSWORD "@WIFI_PASSWORD@"
->>>>>>> 36a06143fdc6b758dadc0187171d57ce41cff37c
 #endif
